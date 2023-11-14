e43e7a386310103b893b711d4c7af1b52b9858e3
czl024 <czedl024@gmail.com>
2023-11-13 20:01:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -29,11 +29,12 @@ lllll
 ll 
 `,
   `
-rrr
- rrr   
-  rrr
- rrr
-rrr
+rrrr
+ rrrr
+  rrrr   
+  rrrr
+ rrrr
+rrrr
 `,
 ];
 
@@ -119,6 +120,8 @@ let boostBreak = 200;
 let boostCollected = false;
 let deathTimer = 2000;
 let fTriangle;
+let level = 1;
+let SPEED_FACTOR = 0.15
 
 //globals
 let ypointtip = 50;
@@ -127,7 +130,11 @@ let ypointbottom = 0;
 
 
 function update() {
+  // startup
   if (!ticks) {
+    SPEED_FACTOR = 0.15
+    speed = 1;
+    level = 1;
     deathTimer = 2000;
     stars = times(20, () => {
       // Random number generator function
@@ -157,12 +164,22 @@ function update() {
       pos: vec(G.WIDTH - 5, rnd(0, G.HEIGHT)),
       speed: speed,
     });
-
   }
+  // /startup
+
+
+
   fTriangle.forEach((t) => {
     updateTriangle(t);
   });
 
+
+
+  text(`Level : ${level}`, 90, 95);
+
+
+
+  // particles
   stars.forEach((s) => {
     // Move the star downwards
     s.pos.x -= s.speed;
@@ -174,7 +191,11 @@ function update() {
     // Draw the star as a square of size 1
     box(s.pos, 1);
   });
+  // /particles
 
+
+
+  // handle input
   if (input.isPressed) holdTime++;
   else if (input.isJustReleased) holdTime = 0;
 
@@ -197,7 +218,11 @@ function update() {
     char("a ", player.pos);
   }
   player.pos.clamp(0, G.WIDTH, 1, G.HEIGHT - 2);
+  // /handle input
 
+
+
+  //idk wtf this is for
   player.firingCooldown--;
   // Time to fire the next shot
   if (player.firingCooldown <= 0) {
@@ -228,14 +253,26 @@ function update() {
     return fb.pos.x < 0;
   });
 
+
+
+  // speed boost collectable
   const speedBoostCollected = char("d", speedBoost.pos).isColliding.rect.yellow;
 
   if (speedBoostCollected && !boostCollected) {
-    speed += 0.1;
-    speedBoost.pos = vec(-5, rnd(0, G.HEIGHT));
+    speed += SPEED_FACTOR;
+    speedBoost.pos = vec(-1, rnd(0, G.HEIGHT));
     boostCollected = true;
     deathTimer = 2000;
-    addScore(10 * speed, player.pos);
+    addScore(2000 * speed * level, player.pos);
+    console.log(speed);
+    
+    //when 3 boosts are collected, increment the level
+    //each level resets speed but makes each speedboost more powerful, "progression"
+    if(speed >= 0.99 + (SPEED_FACTOR * 4)){
+      speed = 1;
+      level++;
+      SPEED_FACTOR += 0.1
+    }
   }
   speedBoost.pos.x -= speed;
   if (speedBoost.pos.x < 0 || boostCollected) {
@@ -248,29 +285,42 @@ function update() {
   }
   char("d", speedBoost.pos);
 
+  addScore(speed * 4 * level);
+
+
+
+  //end the game if collectable not collected for too long
   deathTimer -= speed;
-  if (deathTimer <= 0) end(getHighscoreText());
+  //if (deathTimer <= 0) end(getHighscoreText());
 }
+
+
+
+
 
 function updateTriangle(object) {
   color("purple");
-  line(20 + object.pos.x, ypointbottom, 65 + object.pos.x, ypointtip);
-  line(65 + object.pos.x, ypointtip, 110 + object.pos.x, ypointbottom);
+  line(30 + object.pos.x, ypointbottom, 65 + object.pos.x, ypointtip);
+  line(65 + object.pos.x, ypointtip, 100 + object.pos.x, ypointbottom);
 
   object.pos.x -= speed;
   if (object.pos.x < -110) {
     object.pos.wrap(0, G.WIDTH + 80, 0, G.HEIGHT + 80);
 
     //if it wraps, change coordinates
-    ypointtip = rnd(10, G.HEIGHT-10); //randomizes top of triangle
+    ypointtip = rnd(10, G.HEIGHT-25); //randomizes top of triangle
 
-    let coin = rnd(0,2);            //50/50 chance to spawn triangle at top/bottom
+    let coin = Math.floor(Math.random() * 2);            //50/50 chance to spawn triangle at top/bottom
     if (coin == 0) ypointbottom = 0;
     else ypointbottom = G.HEIGHT;
   }
 
   if (char("c", player.pos).isColliding.rect.purple) end(getHighscoreText());
 }
+
+
+
+
 
 function getHighscoreText(){
   if (score > 0 && Highscores.indexOf(score) == -1 ){
